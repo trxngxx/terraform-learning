d5abcf446461dc886860270e8271ccc79e625430
trxngxx <nnartnav@gmail.com>
2024-05-14 22:14:41 +0700

    init module files

---
+++ module/common/11-security-groups.tf
@@ -0,0 +1,20 @@
+
+resource "aws_security_group" "k8s_sg" {
+  name        = "k8s_sg"
+  description = "Allow all traffic within VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
