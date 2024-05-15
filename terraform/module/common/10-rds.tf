f9e4837dbb6bfed6409947e0235303c115f8ea12
trxngxx <nnartnav@gmail.com>
2024-05-15 09:08:56 +0700

    update module/common/rds.tf file

--- terraform/module/common/10-rds.tf
+++ terraform/module/common/10-rds.tf
@@ -8,9 +8,9 @@ resource "aws_db_instance" "rds" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "mydb"
+  name                 = "database_name"
   username             = "admin"
-  password             = "yourpassword"
+  password             = "password"
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
   skip_final_snapshot  = true
 }
