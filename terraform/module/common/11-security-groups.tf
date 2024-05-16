69c3b7ac2f34452fde2e6bada2b19ad8bd66b8b4
trxngxx <nnartnav@gmail.com>
2024-05-16 16:46:39 +0700

    update terraform/module/common security-groups.tf

--- terraform/module/common/11-security-groups.tf
+++ terraform/module/common/11-security-groups.tf
@@ -18,3 +18,23 @@ resource "aws_security_group" "k8s_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "rds" {
+  name        = "rds_sg"
+  description = "Allow all traffic to RDS"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
